e3cd1973f81619029ca770856e487e9db47b0265
Matheus Victor <matheusvictor2000@gmail.com>
2022-09-30 15:18:37 -0300

    refactored script.js

--- script/script.js
+++ script/script.js
@@ -25,52 +25,71 @@ const toDoForm = document.querySelector('#toDo-form');
 const toDoinput = document.querySelector('#toDo-input');
 const toDoday = document.querySelector('#select-day');
 const hourInput = document.querySelector('#hourInput');
+const buttonRemoveAll = document.querySelector('.filter__form-removeButton');
 const cardsPlanner = document.querySelector('.cards__planner-list');
 const buttonSaveLocalStorage = document.querySelector('.menu__header-item-saveButton');
 const buttonCleanLocalStorage = document.querySelector('.menu__header-item-deleteButton');
+const buttonMonday = document.getElementById('monday')
+const buttonTuesday = document.getElementById('tuesday')
+const buttonWednesday = document.getElementById('wednesday')
+const buttonThursday = document.getElementById('thursday')
+const buttonFriday = document.getElementById('friday')
+const buttonSaturday = document.getElementById('saturday')
+const buttonSunday = document.getElementById('sunday')
 
 // FUNCTIONS
-    const saveTodo = (toDo, toDoday ,hourTodo) => {
-        const toDoCard = document.createElement('li');
-        toDoCard.classList.add('cards__planner-item');
-   
-        const toDoHour = document.createElement('div');
-        toDoHour.classList.add('cards__planner-hour');
-        toDoHour.classList.add(toDoday);
-            
-        const toDoTextHour = document.createElement('h2');
-        toDoTextHour.innerHTML = hourTodo;
+    const schedule = []
 
-        toDoCard.appendChild(toDoHour);
-        toDoHour.appendChild(toDoTextHour);
-            
-        const toDoActivity = document.createElement('div');
-        toDoActivity.classList.add('cards__planner-task');
-        toDoActivity.classList.add(toDoday + 'Before')
-            
-        const toDoTextActivity = document.createElement('p');
-        toDoTextActivity.classList.add('cards__planner-task-text')
-        toDoTextActivity.innerHTML = toDo;
+    function saveTodo(toDo, toDoday ,hourTodo) {
 
-        const toDoActivityRemove = document.createElement('button');
-        toDoActivityRemove.classList.add('cards__planner-task-button');
-        toDoActivityRemove.innerText = 'Apagar';
+        schedule.push({toDo, toDoday , hourTodo})
 
-        toDoCard.appendChild(toDoActivity);
-        toDoActivity.appendChild(toDoTextActivity);
-        toDoActivity.appendChild(toDoActivityRemove);
-      
-        cardsPlanner.appendChild(toDoCard);
-
-        toDoinput.value = "";
-        hourInput.value = "";
-
-        toDoActivityRemove.addEventListener('click' , () => {
-            cardsPlanner.removeChild(toDoCard);
-        }) 
+        renderSchedule();
+        
     }
 
+    function renderSchedule(schedule) {
+
+        cardsPlanner.innerHTML = '';
+
+       
+        for (let index = 0; index < schedule.length; index++) {
+
+            const item = schedule[index]   
+             
+            const toDoCard = document.createElement('li');
+            toDoCard.classList.add('cards__planner-item');
     
+            const toDoHour = document.createElement('div');
+            toDoHour.classList.add('cards__planner-hour');
+            toDoHour.classList.add(item.toDoday);
+                
+            const toDoTextHour = document.createElement('h2');
+            toDoTextHour.innerHTML = item.hourTodo;
+
+            toDoCard.appendChild(toDoHour);
+            toDoHour.appendChild(toDoTextHour);
+                
+            const toDoActivity = document.createElement('div');
+            toDoActivity.classList.add('cards__planner-task');
+            toDoActivity.classList.add(item.toDoday + 'Before')
+                
+            const toDoTextActivity = document.createElement('p');
+            toDoTextActivity.classList.add('cards__planner-task-text')
+            toDoTextActivity.innerHTML = item.toDo;
+
+            const toDoActivityRemove = document.createElement('button');
+            toDoActivityRemove.classList.add('cards__planner-task-button');
+            toDoActivityRemove.innerText = 'Apagar';
+
+            toDoCard.appendChild(toDoActivity);
+            toDoActivity.appendChild(toDoTextActivity);
+            toDoActivity.appendChild(toDoActivityRemove);
+        
+            cardsPlanner.appendChild(toDoCard);
+
+        }
+    }
 
 // EVENTS
 
@@ -83,9 +102,7 @@ toDoForm.addEventListener('submit', (e) => {
     const hourInputValue = hourInput.value;
 
     saveTodo(inputValue, toDodayValue, hourInputValue);
-   
 })
-
 
  buttonSaveLocalStorage.addEventListener('click', () => {
     alert('TESTE');
@@ -93,4 +110,47 @@ toDoForm.addEventListener('submit', (e) => {
 
  buttonCleanLocalStorage.addEventListener('click', () => {
     localStorage.clear();
+ })
+
+ buttonMonday.addEventListener('click' , () => {
+    const filteredArray = schedule.filter((item) => item.toDoday === 'Monday');
+    renderSchedule(filteredArray);
+    toDoinput.value = "";
+    hourInput.value = "";
+ })
+ buttonTuesday.addEventListener('click' , () => {
+    const filteredArray = schedule.filter((item) => item.toDoday === 'Tuesday');
+    renderSchedule(filteredArray);
+    toDoinput.value = "";
+    hourInput.value = "";
+ })
+ buttonWednesday.addEventListener('click' , () => {
+    const filteredArray = schedule.filter((item) => item.toDoday === 'Wednesday');
+    renderSchedule(filteredArray);
+    toDoinput.value = "";
+    hourInput.value = "";
+ })
+ buttonThursday.addEventListener('click' , () => {
+    const filteredArray = schedule.filter((item) => item.toDoday === 'Thursday');
+    renderSchedule(filteredArray);
+    toDoinput.value = "";
+    hourInput.value = "";
+ })
+ buttonFriday.addEventListener('click' , () => {
+    const filteredArray = schedule.filter((item) => item.toDoday === 'Friday');
+    renderSchedule(filteredArray);
+    toDoinput.value = "";
+    hourInput.value = "";
+ })
+ buttonSaturday.addEventListener('click' , () => {
+    const filteredArray = schedule.filter((item) => item.toDoday === 'Saturday');
+    renderSchedule(filteredArray);
+    toDoinput.value = "";
+    hourInput.value = "";
+ })
+ buttonSunday.addEventListener('click' , () => {
+    const filteredArray = schedule.filter((item) => item.toDoday === 'Sunday');
+    renderSchedule(filteredArray);
+    toDoinput.value = "";
+    hourInput.value = "";
  })
