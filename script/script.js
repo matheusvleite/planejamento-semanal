dcd2a66ff98cb93f58cfe32f0aa55cfe3a16b859
Matheus Victor <matheusvictor2000@gmail.com>
2022-09-30 21:02:09 -0300

    created fuction clean input

--- script/script.js
+++ script/script.js
@@ -1,22 +1,22 @@
 // HEADER HOUR AND DATE
 
 setInterval(() => {
-    let dataCurrent = new Date();
-    //HOUR
-    function fix(hour){
-        if(hour < 10) {
-            hour = "0" + hour
-        }
-        return hour;
-    }
-    document.querySelector('.menu__header-item-hour').innerHTML = 
-    (fix(dataCurrent.getHours()) + ':' + fix(dataCurrent.getMinutes()));
-    //DATE
-    day = new Array ('Janeiro', 'Fevereiro', 
-    'Março', 'Abril', 'Maio', 'Junho', 'Julho',
-    'Agosto','Setembro', 'Outubro', 'Novembro', 'Dezembro');
-    document.querySelector('.menu__header-item-date').innerHTML = 
-    (dataCurrent.getDate() + ' de ' + day[dataCurrent.getMonth()] + ' de ' + dataCurrent.getFullYear());
+   let dataCurrent = new Date();
+   //HOUR
+   function fix(hour) {
+      if (hour < 10) {
+         hour = "0" + hour
+      }
+      return hour;
+   }
+   document.querySelector('.menu__header-item-hour').innerHTML =
+      (fix(dataCurrent.getHours()) + ':' + fix(dataCurrent.getMinutes()));
+   //DATE
+   day = new Array('Janeiro', 'Fevereiro',
+      'Março', 'Abril', 'Maio', 'Junho', 'Julho',
+      'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro');
+   document.querySelector('.menu__header-item-date').innerHTML =
+      (dataCurrent.getDate() + ' de ' + day[dataCurrent.getMonth()] + ' de ' + dataCurrent.getFullYear());
 }, 500);
 
 // SELECT ELEMENTS
@@ -37,120 +37,151 @@ const buttonFriday = document.getElementById('friday')
 const buttonSaturday = document.getElementById('saturday')
 const buttonSunday = document.getElementById('sunday')
 
+
 // FUNCTIONS
-    const schedule = []
 
-    function saveTodo(toDo, toDoday ,hourTodo) {
+function clear() {
 
-        schedule.push({toDo, toDoday , hourTodo})
+   toDoinput.value = ""
+   hourInput.value = ""
 
-        renderSchedule();
-        
-    }
+}
 
-    function renderSchedule(schedule) {
+const schedule = []
 
-        cardsPlanner.innerHTML = '';
+function saveTodo(toDo, toDoday, hourTodo) {
 
-       
-        for (let index = 0; index < schedule.length; index++) {
+   schedule.push({ toDo, toDoday, hourTodo })
 
-            const item = schedule[index]   
-             
-            const toDoCard = document.createElement('li');
-            toDoCard.classList.add('cards__planner-item');
-    
-            const toDoHour = document.createElement('div');
-            toDoHour.classList.add('cards__planner-hour');
-            toDoHour.classList.add(item.toDoday);
-                
-            const toDoTextHour = document.createElement('h2');
-            toDoTextHour.innerHTML = item.hourTodo;
+   renderSchedule();
 
-            toDoCard.appendChild(toDoHour);
-            toDoHour.appendChild(toDoTextHour);
-                
-            const toDoActivity = document.createElement('div');
-            toDoActivity.classList.add('cards__planner-task');
-            toDoActivity.classList.add(item.toDoday + 'Before')
-                
-            const toDoTextActivity = document.createElement('p');
-            toDoTextActivity.classList.add('cards__planner-task-text')
-            toDoTextActivity.innerHTML = item.toDo;
+}
 
-            const toDoActivityRemove = document.createElement('button');
-            toDoActivityRemove.classList.add('cards__planner-task-button');
-            toDoActivityRemove.innerText = 'Apagar';
+function renderSchedule(schedule) {
 
-            toDoCard.appendChild(toDoActivity);
-            toDoActivity.appendChild(toDoTextActivity);
-            toDoActivity.appendChild(toDoActivityRemove);
-        
-            cardsPlanner.appendChild(toDoCard);
+   cardsPlanner.innerHTML = '';
 
-        }
-    }
+
+   for (let index = 0; index < schedule.length; index++) {
+
+      const item = schedule[index]
+
+      const toDoCard = document.createElement('li');
+      toDoCard.classList.add('cards__planner-item');
+
+      const toDoHour = document.createElement('div');
+      toDoHour.classList.add('cards__planner-hour');
+      toDoHour.classList.add(item.toDoday);
+
+      const toDoTextHour = document.createElement('h2');
+      toDoTextHour.innerHTML = item.hourTodo;
+
+      toDoCard.appendChild(toDoHour);
+      toDoHour.appendChild(toDoTextHour);
+
+      const toDoActivity = document.createElement('div');
+      toDoActivity.classList.add('cards__planner-task');
+      toDoActivity.classList.add(item.toDoday + 'Before')
+
+      const toDoTextActivity = document.createElement('p');
+      toDoTextActivity.classList.add('cards__planner-task-text')
+      toDoTextActivity.innerHTML = item.toDo;
+
+      const toDoActivityRemove = document.createElement('button');
+      toDoActivityRemove.classList.add('cards__planner-task-button');
+      toDoActivityRemove.innerText = 'Apagar';
+
+      toDoCard.appendChild(toDoActivity);
+      toDoActivity.appendChild(toDoTextActivity);
+      toDoActivity.appendChild(toDoActivityRemove);
+
+      cardsPlanner.appendChild(toDoCard);
+
+      console.log(item.toDo)
+
+   }
+}
+// LOCALSTORAGE
+
+function saveLocalStorage() {
+   alert('.')
+}
 
 // EVENTS
 
 toDoForm.addEventListener('submit', (e) => {
 
-    e.preventDefault();
+   e.preventDefault();
 
-    const inputValue = toDoinput.value;
-    const toDodayValue = toDoday.value;
-    const hourInputValue = hourInput.value;
+   const inputValue = toDoinput.value;
+   const toDodayValue = toDoday.value;
+   const hourInputValue = hourInput.value;
+   clear();
+   saveTodo(inputValue, toDodayValue, hourInputValue);
 
-    saveTodo(inputValue, toDodayValue, hourInputValue);
 })
 
- buttonSaveLocalStorage.addEventListener('click', () => {
-    alert('TESTE');
- })
+buttonRemoveAll.addEventListener('click', () => {
 
- buttonCleanLocalStorage.addEventListener('click', () => {
-    localStorage.clear();
- })
+})
 
- buttonMonday.addEventListener('click' , () => {
-    const filteredArray = schedule.filter((item) => item.toDoday === 'Monday');
-    renderSchedule(filteredArray);
-    toDoinput.value = "";
-    hourInput.value = "";
- })
- buttonTuesday.addEventListener('click' , () => {
-    const filteredArray = schedule.filter((item) => item.toDoday === 'Tuesday');
-    renderSchedule(filteredArray);
-    toDoinput.value = "";
-    hourInput.value = "";
- })
- buttonWednesday.addEventListener('click' , () => {
-    const filteredArray = schedule.filter((item) => item.toDoday === 'Wednesday');
-    renderSchedule(filteredArray);
-    toDoinput.value = "";
-    hourInput.value = "";
- })
- buttonThursday.addEventListener('click' , () => {
-    const filteredArray = schedule.filter((item) => item.toDoday === 'Thursday');
-    renderSchedule(filteredArray);
-    toDoinput.value = "";
-    hourInput.value = "";
- })
- buttonFriday.addEventListener('click' , () => {
-    const filteredArray = schedule.filter((item) => item.toDoday === 'Friday');
-    renderSchedule(filteredArray);
-    toDoinput.value = "";
-    hourInput.value = "";
- })
- buttonSaturday.addEventListener('click' , () => {
-    const filteredArray = schedule.filter((item) => item.toDoday === 'Saturday');
-    renderSchedule(filteredArray);
-    toDoinput.value = "";
-    hourInput.value = "";
- })
- buttonSunday.addEventListener('click' , () => {
-    const filteredArray = schedule.filter((item) => item.toDoday === 'Sunday');
-    renderSchedule(filteredArray);
-    toDoinput.value = "";
-    hourInput.value = "";
- })
+// BUTTONS LOCALSTORAGE
+buttonSaveLocalStorage.addEventListener('click', () => {
+   saveLocalStorage()
+})
+
+buttonCleanLocalStorage.addEventListener('click', () => {
+   localStorage.clear();
+})
+
+//BUTTONS DAYS
+
+buttonMonday.addEventListener('click', () => {
+
+   const filteredArray = schedule.filter((item) => item.toDoday === 'Monday');
+   renderSchedule(filteredArray);
+
+})
+
+buttonTuesday.addEventListener('click', () => {
+
+   const filteredArray = schedule.filter((item) => item.toDoday === 'Tuesday');
+   renderSchedule(filteredArray);
+
+})
+
+buttonWednesday.addEventListener('click', () => {
+
+   const filteredArray = schedule.filter((item) => item.toDoday === 'Wednesday');
+   renderSchedule(filteredArray);
+
+})
+
+buttonThursday.addEventListener('click', () => {
+
+   const filteredArray = schedule.filter((item) => item.toDoday === 'Thursday');
+   renderSchedule(filteredArray);
+
+})
+
+buttonFriday.addEventListener('click', () => {
+
+   const filteredArray = schedule.filter((item) => item.toDoday === 'Friday');
+   renderSchedule(filteredArray);
+
+})
+
+buttonSaturday.addEventListener('click', () => {
+
+   const filteredArray = schedule.filter((item) => item.toDoday === 'Saturday');
+   renderSchedule(filteredArray);
+
+})
+
+buttonSunday.addEventListener('click', () => {
+
+   const filteredArray = schedule.filter((item) => item.toDoday === 'Sunday');
+   renderSchedule(filteredArray);
+
+})
+
